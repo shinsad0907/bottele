7a1fdabff828707119854b0ea88996e394ad1433
shinsad0907 <112173054+shinsad0907@users.noreply.github.com>
2026-03-18 06:37:01 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -83,8 +83,8 @@ def webhook_info():
 def getkey():
 
     id_key = request.args.get("id_key")
-    if not id_key:
-        return "Missing id_key parameter", 400
+    # if not id_key:
+    #     return "Missing id_key parameter", 400
 
     res = supabase.table("external_link")\
         .select("*")\
@@ -104,10 +104,10 @@ def getkey():
 @app.route("/result_key")
 def result_key():
     key = request.args.get("key")
-    if not key:
-        return "Missing key parameter", 400
+    # if not key:
+    #     return "Missing key parameter", 400
 
-    return render_template("getkey.html", link=key)
+    return render_template("result.html", link=key)
 
 if __name__ == "__main__":
     app.run("index.html", debug=True, port=5000)
